05a9401c24c424144026fe39feff2d7ebd7ae2d8
Lucas Ferreira <lucasferreira059@gmail.com>
2026-09-01 15:50:14 -0300

    criando a tela de login

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,109 @@
+import { useForm } from "react-hook-form";
+import { email, z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAuth } from "../../hooks/useAuth";
+
+const loginSchema = z.object({
+    email: z
+        .string()
+        .email("digite um email válido"),
+
+    senha: z
+    .string()
+    .min(1, "Digite sua senha."),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
+
+export default function Login() {
+    const { login, loading } = useAuth();
+
+    const { register, handleSubmit, formState: { errors },} = useForm<LoginFormData>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    async function onSubmit(data: LoginFormData) {
+        try {
+            await login({
+                email: data.email,
+                senha: data.senha,
+            });
+
+            console.log("Login realizado com sucesso!");
+            
+        } catch (error) {
+            console.error("erro ao fazer login:", error);
+        }
+    }
+
+    return (
+        <div className="min-w-screen bg-gray-100 flex items-center justify-center px-4" >
+            <div className=" w-full max-w-md " >
+                <div className=" bg-white rounded-2xl shadow-lg p-8 " >
+                    <div className=" text-center mb-8 " >
+                        <h1 className="text-3xl font-bold text-gray-900" >Gestão para psicólogos</h1>
+                        <p className=" text-gray-500 mt-2 " >Entre na sua conta</p>
+                    </div>
+                    <form 
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="space-y-5" 
+                    >
+                        {/* {EMAIL} */}
+                        <div>
+                            <label 
+                                htmlFor="email"
+                                className=" block text-sm font-medium text-gray-700 mb-1 "
+                                >
+                                    Email
+                            </label>
+                            <input 
+                                id="email"
+                                type="email"
+                                placeholder="seu@email.com"
+                                {... register("email")}
+                                className=" w-full border border-gray-300 rounded-lg px-4 py-2.5 outline-none focus:ring-blue-500 focus:border-blue-500 "
+                            />
+                            {errors.email && (
+                                <p className="text-red-500 text-sm mt-1" >
+                                    {errors.email.message}
+                                </p>
+                            )}
+                        </div>
+
+                        {/* {SENHA} */}
+                        <div>
+                            <label 
+                                htmlFor="senha"
+                                className="block text-sm font-medium text-gray-700 mb-1"
+                                >
+                                    Senha
+                                </label>
+                            <input
+                                id="senha" 
+                                type="text"
+                                placeholder="Digite sua senha"
+                                {... register("senha")}
+                                className=" w-full border border-gray-300 rounded-lg px-4 py-2.5 outline-none focus:ring-blue-500 focus:border-blue-500 " 
+                            />
+                            {errors.senha && (
+                                <p className="text-red-500 text-sm mt-1" >
+                                    {errors.senha.message}
+                                </p>
+                            )}
+                        </div>
+
+                        {/* {BOTÃO} */}
+                        <button
+                            type="submit"
+                            disabled={loading}
+                            className="w-full bg-blue-600 text-white rounded-lg py-2.5 font-medium hover:bg-blue-700 transition disabled:opacity-50 disabled:cursor-not-allowed"
+                        >
+                            {loading ? "Entrando..." : "Entrar"}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
